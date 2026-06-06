afbc0d58cb8a9cfb4d7b0fef33b2bc13821b8134
Wendy Pang <wendypang@Wendys-iMac.local>
2026-06-06 23:18:32 +0100

    Move player movement and collision to server

--- server.js
+++ server.js
@@ -20,15 +20,30 @@ const playerColours = [
   "orange"
 ];
 
+const birdSize = 40;
+const gameWidth = 420;
+const gameHeight = 500;
+
+const gravity = 0.45;
+const flapStrength = -7.8;
+const sideFlapStrength = -6.4;
+const horizontalPush = 4.8;
+const horizontalDrag = 0.92;
+
+const diveAmount = 45;
+const diveRecoveryDelay = 140;
+
+const victimKnockback = 45;
+const attackerRecoil = 15;
+
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-function getPlayersInRoom(roomCode) {
-  if (!rooms[roomCode]) return [];
-  return Object.values(rooms[roomCode].players);
+function randomNumber(min, max) {
+  return Math.floor(min + Math.random() * (max - min + 1));
 }
 
 function generateObstaclePlan(count) {
@@ -43,17 +58,15 @@ function generateObstaclePlan(count) {
       bottomPercent = randomNumber(10, 30) / 100;
     }
 
-    plan.push({
-      topPercent,
-      bottomPercent
-    });
+    plan.push({ topPercent, bottomPercent });
   }
 
   return plan;
 }
 
-function randomNumber(min, max) {
-  return Math.floor(min + Math.random() * (max - min + 1));
+function getPlayersInRoom(roomCode) {
+  if (!rooms[roomCode]) return [];
+  return Object.values(rooms[roomCode].players);
 }
 
 function addPlayerToRoom(socket, roomCode, playerName) {
@@ -66,10 +79,164 @@ function addPlayerToRoom(socket, roomCode, playerName) {
     colour: playerColours[playerCount % playerColours.length],
     x: 70 + playerCount * 55,
     y: 220,
-    alive: true
+    velocityX: 0,
+    velocityY: 0,
+    alive: true,
+    lastInputDirection: null
   };
 
   socket.join(roomCode);
+}
+
+function keepPlayerInsideArena(player) {
+  if (player.x < 0) {
+    player.x = 0;
+    player.velocityX = 0;
+  }
+
+  if (player.x > gameWidth - birdSize) {
+    player.x = gameWidth - birdSize;
+    player.velocityX = 0;
+  }
+
+  if (player.y < 0) {
+    player.y = 0;
+  }
+
+  if (player.y > gameHeight - birdSize) {
+    player.y = gameHeight - birdSize;
+  }
+}
+
+function applyInput(player, direction) {
+  player.lastInputDirection = direction;
+
+  if (direction === "up") {
+    player.velocityY = flapStrength;
+  }
+
+  if (direction === "left") {
+    player.velocityY = sideFlapStrength;
+    player.velocityX -= horizontalPush;
+  }
+
+  if (direction === "right") {
+    player.velocityY = sideFlapStrength;
+    player.velocityX += horizontalPush;
+  }
+
+  if (direction === "down") {
+    player.y += diveAmount;
+    player.velocityY = 2;
+
+    setTimeout(() => {
+      if (player.alive) {
+        player.velocityY = flapStrength;
+      }
+    }, diveRecoveryDelay);
+  }
+}
+
+function updatePlayerPhysics(room) {
+  const players = Object.values(room.players);
+
+  for (const player of players) {
+    if (!player.alive) continue;
+
+    player.velocityY += gravity;
+    player.y += player.velocityY;
+
+    player.x += player.velocityX;
+    player.velocityX *= horizontalDrag;
+
+    keepPlayerInsideArena(player);
+  }
+}
+
+function applyPlayerCollisions(room) {
+  const players = Object.values(room.players).filter(player => player.alive);
+
+  for (let i = 0; i < players.length; i++) {
+    for (let j = i + 1; j < players.length; j++) {
+      const a = players[i];
+      const b = players[j];
+
+      const aCenterX = a.x + birdSize / 2;
+      const aCenterY = a.y + birdSize / 2;
+      const bCenterX = b.x + birdSize / 2;
+      const bCenterY = b.y + birdSize / 2;
+
+      const dx = bCenterX - aCenterX;
+      const dy = bCenterY - aCenterY;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance > 0 && distance < birdSize) {
+        const normalX = dx / distance;
+        const normalY = dy / distance;
+
+        const aSpeed = Math.sqrt(a.velocityX * a.velocityX + a.velocityY * a.velocityY);
+        const bSpeed = Math.sqrt(b.velocityX * b.velocityX + b.velocityY * b.velocityY);
+
+        let attacker = a;
+        let victim = b;
+        let directionX = normalX;
+        let directionY = normalY;
+
+        if (bSpeed > aSpeed) {
+          attacker = b;
+          victim = a;
+          directionX = -normalX;
+          directionY = -normalY;
+        }
+
+        victim.x += directionX * victimKnockback;
+        victim.y += directionY * victimKnockback;
+
+        attacker.x -= directionX * attackerRecoil;
+        attacker.y -= directionY * attackerRecoil;
+
+        victim.velocityX += directionX * 5;
+        victim.velocityY += directionY * 5;
+
+        attacker.velocityX -= directionX * 2;
+        attacker.velocityY -= directionY * 2;
+
+        keepPlayerInsideArena(victim);
+        keepPlayerInsideArena(attacker);
+      }
+    }
+  }
+}
+
+function broadcastPlayerStates(roomCode) {
+  const room = rooms[roomCode];
+
+  if (!room) return;
+
+  io.to(roomCode).emit("playersUpdated", {
+    players: getPlayersInRoom(roomCode)
+  });
+}
+
+function startGameLoop(roomCode) {
+  const room = rooms[roomCode];
+
+  if (!room) return;
+
+  if (room.gameLoop) {
+    clearInterval(room.gameLoop);
+  }
+
+  room.gameLoop = setInterval(() => {
+    if (!rooms[roomCode] || !rooms[roomCode].started) {
+      clearInterval(room.gameLoop);
+      return;
+    }
+
+    updatePlayerPhysics(room);
+    applyPlayerCollisions(room);
+    broadcastPlayerStates(roomCode);
+  }, 1000 / 60);
 }
 
 io.on("connection", (socket) => {
@@ -78,7 +245,8 @@ io.on("connection", (socket) => {
       hostId: socket.id,
       players: {},
       started: false,
-      obstaclePlan: []
+      obstaclePlan: [],
+      gameLoop: null
     };
 
     addPlayerToRoom(socket, roomCode, playerName);
@@ -126,21 +294,20 @@ io.on("connection", (socket) => {
     io.to(roomCode).emit("gameStarting", {
       obstaclePlan: room.obstaclePlan
     });
+
+    startGameLoop(roomCode);
   });
 
-  socket.on("playerState", ({ roomCode, x, y }) => {
+  socket.on("playerInput", ({ roomCode, direction }) => {
     const room = rooms[roomCode];
 
     if (!room || !room.players[socket.id]) return;
 
-    room.players[socket.id].x = x;
-    room.players[socket.id].y = y;
+    const player = room.players[socket.id];
 
-    socket.to(roomCode).emit("playerMoved", {
-      id: socket.id,
-      x,
-      y
-    });
+    if (!room.started || !player.alive) return;
+
+    applyInput(player, direction);
   });
 
   socket.on("disconnect", () => {
@@ -157,6 +324,10 @@ io.on("connection", (socket) => {
         });
 
         if (Object.keys(room.players).length === 0) {
+          if (room.gameLoop) {
+            clearInterval(room.gameLoop);
+          }
+
           delete rooms[roomCode];
         }
       }
